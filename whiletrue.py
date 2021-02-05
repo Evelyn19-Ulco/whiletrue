657077aa27b639236f0480c959a9f2a12d987ec7
Evelyn19-Ulco <76453340+Evelyn19-Ulco@users.noreply.github.com>
2021-02-05 11:48:14 -0500

    Add files via upload

---
+++ whiletrue.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 25 15:24:43 2021
+
+@author: DELL
+"""
+
+x=input ('Ingrese el número que contaré')
+x=int(x)
+y=1
+while True:
+    print (y)
+    y+=1
+    if y>x:
+        break
